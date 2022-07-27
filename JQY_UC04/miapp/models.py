ca5f5df378e262c8da5b74a805a34fb6dbccdf1c
Frank Quintana <franhkquintana@gmail.com>
2022-07-27 10:36:10 -0500

    Parte 2 quintana- tabla estudiante - ok

--- JQY_UC04/miapp/models.py
+++ JQY_UC04/miapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+#creamos modelo de estudiante
+class Estudiante(models.Model):
+    codigo = models.TextField(max_length=10)
+    dni = models.TextField(max_length=8)
+    nombre = models.TextField()
+    apepat = models.TextField()
+    apemat = models.TextField()
+    direccion = models.TextField()
+    telefono = models.TextField()
+    estado = models.CharField(max_length=1)
--- JQY_UC04/miapp/views.py
+++ JQY_UC04/miapp/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import redirect, render, HttpResponse
 # Create your views here.
 layout = """"""
 def integrantes(request):
-    integrantes = ['Jose Flores Chamba','Edson Alva Chanta','Miguel Angel Motta Mendoza']
+    integrantes = ['Julca Espillco Humberto','Quintana Quispe Alegria','Yauricasa mendoza Miguel']
     return render(request, 'integrantes.html', {
         'titulo':'Integrantes del proyecto',
         'integrantes':integrantes
@@ -11,6 +11,6 @@ def integrantes(request):
 
 def saludo(request):
     return render(request, 'saludo.html', {
-        'titulo':'Bienvenidos',
-        'mensaje':'Proyecto para la Unidad de Competencia 04 (UC04)'
+        'titulo':'Bienvenidos a la UNTELS',
+        'mensaje':'Proyecto para la Unidad de Evaluativa N°4'
     })
