3467f578d73f0c72ab5e73b92b3178cc4e1e1f40
Miguel <miguel10mig@gmail.com>
2022-07-27 11:24:49 -0500

    PARTE03PARTE03

--- JQY_UC04/miapp/views.py
+++ JQY_UC04/miapp/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import redirect, render, HttpResponse
+from miapp.models import Estudiante
 # Create your views here.
 layout = """"""
 def integrantes(request):
@@ -14,3 +15,21 @@ def saludo(request):
         'titulo':'Bienvenidos a la UNTELS',
         'mensaje':'Proyecto para la Unidad de Evaluativa N°4'
     })
+# creamos estudiantes
+def crear_estudiante(request):
+    estudiante=Estudiante(
+    codigo = "137258369",
+    dni = "69788869",
+    nombre="Humberto",
+    apepat="Julca",
+    apemat="Espillco",
+    direccion="Jiron de la Union",
+    telefono="955525874",
+    estado="A"
+    )
+    estudiante.save()
+    return HttpResponse (f"<h1>Estudiante registrado:</h1> "+
+    f"<br> <b>Código:</b> {estudiante.codigo} <br> <b>DNI:</b> {estudiante.dni} <br> <b>Nombre:</b> {estudiante.nombre} <br> "+
+    f"<b>Apellido paterno:</b> {estudiante.apepat} <br> <b>Apellido materno:</b> {estudiante.apemat} <br><b>Dirección:</b> "+
+    f" {estudiante.direccion} <br> <b>Telefono:</b> {estudiante.telefono} <br> <b>Estado:</b> {estudiante.estado}")
+    
